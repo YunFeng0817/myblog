f3244415b75e3711d344d6b6e80abca9a12f0fa8
Fitz <f18846188605@gmail.com>
2017-08-13 23:07:31 +0800

    I create the models.py , complete basic objects,and change the timezone in settings.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+#this is information about blog owner
+class blogger(models.Model):
+    blogID = models.CharField(max_length=30,primary_key=True)      #set the key
+    blogName = models.CharField(max_length=30)
+    createTime = models.DateField(timezone.datetime.now())
+
+    def __str__(self):
+        return self.blogID
+
+
+class blogs(models):
+    title = models.CharField(max_length=100)
+    content = models.CharField
+    author = models.ForeignKey(blogger)     #一篇日志可以对应一个作者，但一个作者可以写多个日志 ，故用foreignkey    一本书由多个作者编写，一个作者可以写很多本书，故用manytomanyField
+    writDate = models.DateField
+
+    def __init__(self):
+        self.writDate = timezone.datetime.now()
+
+    def __str__(self):
+        return self.title
+
+
--- myblog/settings.py
+++ myblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from django.utils import timezone
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = timezone.get_current_timezone_name()      #这是正确的使用时区的做法
 
 USE_I18N = True
 
