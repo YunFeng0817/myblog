0050f7a0cb3f9b40aa04a20ede7bcd9d5715782c
Fitz <f18846188605@gmail.com>
2017-08-19 19:13:29 +0800

    I complete the login ,logout,learn to use User model to keep the password safer

--- blog/models.py
+++ blog/models.py
@@ -1,21 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User   #用于扩展已有的用户模型
 # Create your models here.
 
 #this is information about blog owner
-class blogger(models.Model):
-    blogID = models.CharField(max_length=30,primary_key=True)      #set the key
-    blogName = models.CharField(max_length=30)
-    createTime = models.DateField(timezone.datetime.now())
-
-    def __str__(self):
-        return self.blogID
-
-
 class blogs(models.Model):
     title = models.CharField(max_length=100)
     content = models.CharField(max_length=10000)
-    author = models.ForeignKey(blogger)     #一篇日志可以对应一个作者，但一个作者可以写多个日志 ，故用foreignkey    一本书由多个作者编写，一个作者可以写很多本书，故用manytomanyField
+    author = models.ForeignKey(User)     #一篇日志可以对应一个作者，但一个作者可以写多个日志 ，故用foreignkey    一本书由多个作者编写，一个作者可以写很多本书，故用manytomanyField
     writDate = models.DateField(timezone.datetime.now())
 
     def __int__(self):
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.template import RequestContext
 from blog.models import *
 # Create your views here.
+
+#负责主页的视图
 def main(request):
-    blogOwner = blogger.objects.get(blogID='294889365')
-    Name = blogOwner.blogName
-    return render(request,'blog/main.html',{'name':Name})
+    if not request.user.is_authenticated():
+        return render(request,'blog/main.html',)
+    else:
+        user = request.user
+        name = user.username
+        # request_context = RequestContext(request,{'name':'您是访客'})
+        return render(request,'blog/main.html',{'name':name})
+
+#负责登录的动作
+def loginAction(request):
+    if request.method=="POST":
+        blogID = request.POST['blogID']
+        password = request.POST['password']
+        user = authenticate(username=blogID, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                name =user.get_short_name()
+                return render(request,'blog/main.html',{'name':name})
+            else:
+                return render(request,'blog/main.html',{'error':'无效的账号'})
+    return render(request,'blog/main.html',{'error':'密码错误'})
+
+#负责登出的动作
+def logoutAction(request):
+    logout(request)
+    return redirect('/') #render(request,'blog/main.html')
