69bd4f8cbc919625209b5b1a707ca8e74f0f65b2
Fitz <f18846188605@gmail.com>
2017-09-05 15:26:38 +0800

    version for server test 10

--- myblog/settings.py
+++ myblog/settings.py
@@ -15,7 +15,7 @@ from django.utils import timezone
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ESSAYS_DIR = os.path.join(BASE_DIR,'static')
+ESSAYS_DIR = os.path.join(BASE_DIR,'upLoad')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
